ce649fe9cbaa66a252313662f1cc45028bd28c87
weijie <weijie@mavericks-consulting.com>
2023-09-06 15:45:06 +0800

    a

--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -3,5 +3,4 @@ import { Sequelize } from 'sequelize'
 export const sequelize = new Sequelize('weijie-mavericks', 'postgres', '1234', {
     host: 'db',
     dialect: 'postgres',
-    port: 5433,
 })
